469a88e5bcb6b7bbbcbc1b19cefe0bc56d203e68
Palakgupta2002 <pg948303@gmail.com>
2023-04-02 00:12:42 +0530

    Get a random joke

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const jokecontainer=document.getElementById("jokecontainer");
 const button=document.getElementById("button");
-const 
+const url="https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,explicit&type=single";
+let getjoke = ()=>{
+    fetch(url).then
+    (response=>response.json()).then(data=>
+        {
+            jokecontainer.textContent=`${data.joke}`
+        });
+       
+
+        
+}
+button.addEventListener("click",getjoke());
+getjoke();
+
+
+   
